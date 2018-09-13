7e607f83e324beb1afcc7d95a0e0c42c8236cf75
Philip <philipfoster462@gmail.com>
2018-09-13 13:58:03 -0500

    Add hello world controller

---
+++ src/main/java/com/capitalone/creditocr/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.capitalone.creditocr;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * This is a simple hello world endpoint to test the application.
+ *
+ * TODO: remove this
+ */
+@RestController
+public class HelloWorldController {
+
+    String processRequest() {
+        return "Hello World";
+    }
+}
