87765a959b9e2b44189f93d9a2f111428a85b55e
Philip <philipfoster462@gmail.com>
2018-09-13 13:54:02 -0500

    Generated new spring boot project

---
+++ src/main/java/com/capitalone/creditocr/CreditOcrApplication.java
@@ -0,0 +1,12 @@
+package com.capitalone.creditocr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditOcrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditOcrApplication.class, args);
+    }
+}
