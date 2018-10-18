aaad10a8ef6764e855f054f4c8354e98b24c034b
Jacob <jaccam96@gmail.com>
2018-10-17 22:27:03 -0500

    Created skeleton of OCR ingestion method.

--- src/main/java/com/capitalone/creditocr/model/ingest/ByteIngester.java
+++ src/main/java/com/capitalone/creditocr/model/ingest/ByteIngester.java
@@ -1,19 +1,28 @@
 package com.capitalone.creditocr.model.ingest;
 
 import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.TesseractException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ByteIngester {
-    ITesseract tess;
+    private ITesseract tess;
+    private String output;
 
     @Autowired
     public ByteIngester(ITesseract t) {
         tess = t;
     }
 
+    //TODO: doOCR accepts type File. Must convert input to File type.
     public String ingest(byte[] byteArray) {
+        try {
+            output = tess.doOCR();
+            System.out.println("Output:\n" + output);
+        } catch (TesseractException e){
+            System.err.println(e.getMessage());
+        }
         return null;
     }
 
