e6f03f88f2291a8e916749bdc5043a0b642630cd
Philip <philipfoster462@gmail.com>
2018-10-23 17:15:30 -0500

    Update run scripts to not continue if the compile fails.

--- run.bat
+++ run.bat
@@ -2,6 +2,13 @@
 REM This script will compile the project, build the docker image, and launch it with all necessary dependencies
 
 call mvnw package
-REM docker-compose build
+if %errorlevel% NEQ 0 {
+    echo.
+    echo.
+    echo                              COULD NOT COMPILE PROJECT
+    echo.
+    echo.
+    exit /b %errorlevel%
+}
 docker build -t credit-ocr --build-arg JAR_FILE=./target/credit-ocr-0.0.1-SNAPSHOT.jar .
 docker-compose up
