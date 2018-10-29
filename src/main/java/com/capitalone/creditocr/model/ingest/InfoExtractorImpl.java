d29fa42f80a44363b4c75ef5953f9d9198b632a9
Austin <abk150030@utdallas.edu>
2018-10-28 20:21:04 -0500

    Committed InfoExtractor interface and basic shell of InfoExtractorImpl class for implementation. This was done so the other team members could see the work so far.

---
+++ src/main/java/com/capitalone/creditocr/model/ingest/InfoExtractorImpl.java
@@ -0,0 +1,28 @@
+package com.capitalone.creditocr.model.ingest;
+
+public class InfoExtractorImpl implements InfoExtractor {
+
+    public InfoExtractorImpl() {
+        //TODO
+    }
+
+    @Override
+    public void extractAddress() {
+        //TODO
+    }
+
+    @Override
+    public void extractDate() {
+        //TODO
+    }
+
+    @Override
+    public void extractName() {
+        //TODO
+    }
+
+    @Override
+    public void extractNumbers() {
+        //TODO
+    }
+}
